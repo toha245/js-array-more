bb20bb137bd831ab6fc8660bb1d087e888c675fe
toha245 <mdabdulawal2001@gmail.com>
2025-10-06 00:00:10 +0600

    first commit

---
+++ sort.js
@@ -0,0 +1,18 @@
+const persons = ['rakib', 'nokib', 'sakib', 'akib', 'dakib'];
+const sortedPerson = persons.sort();
+console.log(sortedPerson);
+
+// sort --> 
+// const numbers = [4,7,2,8,3,6];
+const numbers = [4,7,12,8,43,6,1];
+
+/* 
+Ascending --> smaller to larger: [2,3,4,6,7,8]
+Descending --> larger to smaller: [8,7,6,4,3,2]
+*/
+
+// const numbers_asc = numbers.sort(); // not working
+const numbers_asc = [...numbers].sort(function (a, b) {return a - b});
+const numbers_dsc = [...numbers].sort(function (a, b) {return b - a});
+console.log(numbers_asc);
+console.log(numbers_dsc);
